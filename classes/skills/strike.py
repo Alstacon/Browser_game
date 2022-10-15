d0224a1aab4e553d3c5e36b919d32f2906879288
alstacon <alstacon@gmail.com>
2022-10-15 18:12:11 +0400

    created classes up to the units/ equipment needs to be completed

---
+++ classes/skills/strike.py
@@ -0,0 +1,14 @@
+from classes.abstract_skills import Skill
+
+
+class BlinkStrike(Skill):
+    name = "Blink Strike"
+    damage = 100
+    stamina = 65
+
+    def skill_effect(self) -> str:
+        self.user.stamina -= self.stamina
+        self.target.health -= self.damage
+        return f"""{self.user.name} телепортируется за спину врага и наносит ему урон."""
+
+
---
+++ classes/units.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class UnitClass:
+    name: str
+    max_health: float
+    max_stamina: float
+    attack: float
+    stamina: float
+    armor: float
+    skill: ConcreteSkill
+
+
+Warrior = UnitClass()
+Thief = UnitClass()
